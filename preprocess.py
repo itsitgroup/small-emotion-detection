c6c61f68b22b40e32b44a94bf295c5d58fe45279
V12Hero <ammar.ahmad20@yahoo.com>
2024-05-30 08:39:50 +0500

    added preprocess

---
+++ preprocess.py
@@ -0,0 +1,54 @@
+import pyaudio
+import wave
+import librosa
+import numpy as np
+import pandas as pd
+
+def audio_recorder():
+    CHUNK = 1024 
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2 
+    RATE = 44100 
+    RECORD_SECONDS = 4
+    WAVE_OUTPUT_FILENAME = "output10.wav"
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print("* recording")
+
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print("* done recording")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+def preprocessing():
+    data, sampling_rate = librosa.load('output10.wav')
+    X, sample_rate = librosa.load('output10.wav', res_type='kaiser_fast', duration=2.5, sr=22050*2, offset=0.5)
+    sample_rate = np.array(sample_rate)
+    mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=13), axis=0)
+    featurelive = mfccs
+    livedf2 = featurelive
+    livedf2 = pd.DataFrame(data=livedf2)
+    livedf2 = livedf2.stack().to_frame().T
+    twodim = np.expand_dims(livedf2, axis=2)
+    return twodim
